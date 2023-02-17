2b86d2f0d02844c2621485b0c5afc9cc72706506
giannisjv <giannisvidras@gmail.com>
2023-02-17 10:44:47 +0200

    scaling code

---
+++ code/graphical tranformations/scaling/scaling.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "../../myLibs/functions.h"
+#include "../../myLibs/colib.h"
+#include "../../myLibs/parallelFunctions.h"
+
+int main(int argc, char const *argv[])
+{
+    int i,j;
+
+
+    
+    return 0;
+}
